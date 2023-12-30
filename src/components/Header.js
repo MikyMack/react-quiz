3bf1c571afe935ebf91747e075abcc3ae8998327
micky mack <jijomonjs@gmail.com>
2023-12-30 15:34:56 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <header className="app-header">
+      <img
+        src="https://image.similarpng.com/very-thumbnail/2020/12/Quiz-comic-pop-art-style-illustration-on-transparent-background-PNG.png"
+        alt="jijo's quiz"
+      />
+      <h1>The React Quiz</h1>
+    </header>
+  );
+}
+
+export default Header;
